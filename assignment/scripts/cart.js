b649fac4e5fa1973f52f2fdff58cf2a77113979b
Miski Ali <miskiali222@gmail.com>
2022-05-29 17:34:48 -0500

    I did all the questions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,41 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+let basket = [];
+// create basket and add an item function
+console.log( basket );
+
+function addItem(item){
+    basket.push(item) 
+    //new item should be added to the array
+    return true;
+}
+
+// a console.log that tests whats happening
+
+console.log ('adding ball', addItem('ball'));
+console.log ('adding toy', addItem('toy'));
+console.log ('adding book', addItem('book'));
+
+
+// start listItems function 
+
+function listItems () {
+    
+}
+console.log('Items that are in my basket:');
+listItems();
+
+//check if items are listed 
+console.log(`My basket has a ${basket}`);
+
+function empty () {
+    basket = [];
+}
+// should remove item from basket
+console.log ('Remove all item from my basket', empty());
+
+//check if items were actually emptied
+console.log(`basket is ${basket}`);
